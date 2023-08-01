8e85d90712af452a294e8285c584ead36de935c4
JaiTolentino <jaitolentinoo@gmail.com>
2023-08-01 23:16:37 +0800

    fix: fixed working environment

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const express = require('express');
 const app = express();
-const engine = require('consolidate');
+const mainroute = require('./controller/main_routes');
+app.use('views', express.static('views'));
+app.set('view engine', 'hbs');
+app.use(express.static('views/assets'));
+app.use('/images', express.static('images'));
+app.use(express.static('views'));
+app.use('/', mainroute);
+app.listen(4000, () => {
+    console.log("Website started at port 4000");
+})
