00585ad1c5b47cb716906fd64ef5d105511bd70c
JaiTolentino <jaitolentinoo@gmail.com>
2023-08-03 14:51:04 +0800

    feat: finishing up backend

--- controller/server_posts.js
+++ controller/server_posts.js
@@ -5,6 +5,7 @@ const admin_connection = require('./admin_connection');
 const parser = require('body-parser');
 const mainController = require('./maincontroller');
 const Admin = require('../model/Admin');
+const Resident = require('../model/HeadResident');
 
 app.post("/",parser.urlencoded({extended: true}), (req, res) => {
     const {username, passwords} = req.body;
@@ -14,8 +15,7 @@ app.post("/",parser.urlencoded({extended: true}), (req, res) => {
             else{
                 if(result.length > 0){
                     if(mainController.comparePassword(passwords, result[0]['encrypted_password'])){
-                        const user = new Admin(result[0]['id'], result[0]['fname'], result[0]['lname'], result[0]['birthdate'],
-                            result[0]['position'], result[0]['username'], result[0]['encrypted_password']);
+                        const user = new Admin(result[0]['user_id'], result[0]['username'], result[0]['position'], result[0]['encrypted_password']);
                             req.session.kapitan = user;
                             res.redirect('/kapitan/home');
                     }else {
@@ -32,8 +32,7 @@ app.post("/",parser.urlencoded({extended: true}), (req, res) => {
             else{
                 if(result.length > 0){
                     if(mainController.comparePassword(passwords, result[0]['encrypted_password'])){
-                        const user = new Admin(result[0]['id'], result[0]['fname'], result[0]['lname'], result[0]['birthdate'],
-                            result[0]['position'], result[0]['username'], result[0]['encrypted_password']);
+                        const user = new Admin(result[0]['user_id'], result[0]['username'], result[0]['position'], result[0]['encrypted_password']);
                         req.session.admin = user;
                         res.redirect('/admin/home');
                     }else {
@@ -81,7 +80,129 @@ app.post("/add-admin", parser.urlencoded({extended: true}), (req, res) => {
     }
 })
 app.post("/add-resident", parser.urlencoded({extended: true}), (req, res) => {
-    console.log(req.body);
+    if (req.session.kapitan){
+        const admin = new Admin(req.session.kapitan.admin_id, req.session.kapitan.username,
+            req.session.kapitan.position, req.session.kapitan.encrypted_password);
+        const {firstName,
+            lastName,
+            addressline1,
+            addressline2, 
+            addressbarangay,
+            addresscity,
+            addresscountry,
+            birthday,
+            gender,
+            contact,
+            status,
+            remarks,
+            medicalCondition,
+            voter,
+            pwd} = req.body;
+            const one = mainController.createAdminId(firstName);
+            const two = mainController.createAdminId(lastName);
+            const user_id = one+two;
+            const resident = new Resident(user_id,
+                                            firstName,
+                                            lastName,
+                                            contact,
+                                            birthday,
+                                            gender,
+                                            addressline1,
+                                            addressline2,
+                                            addressbarangay,
+                                            addresscountry,
+                                            voter,
+                                            pwd,
+                                            medicalCondition,
+                                            status,
+                                            remarks,
+                                            new Date(),
+                                            admin.getUsername()
+                                            );
+            kapitan_connection.query("INSERT INTO masterlist (resident_id, fname, lname, contact, birthdate, gender, age, address, is_voter, is_pwd, medical_condition, status, remarks, created_by) VALUES (?, ?,?,?,?,?,?,?,?,?,?,?,?,?)",[
+                resident.getResidentId(),
+                resident.getFname(),
+                resident.getLname(),
+                resident.getContactNum(),
+                resident.getBirthdate(),
+                resident.getGender(),
+                resident.getAge(),
+                resident.getAddress().getFullAddress(),
+                resident.getIsVoter(),
+                resident.getIsPwd(),
+                resident.getMedicalCondition().getDisease(),
+                resident.getStatus(),
+                resident.getRemarks(),
+                admin.getUsername()
+            ], (err, results) => {
+                if (err) console.log(err);
+                else {
+                    res.render('kapitan/kapitan-add-residents', {AlertMessage: "Resident Recorded"})
+                }
+            })
+    }else if (req.session.admin){
+        const admin = new Admin(req.session.admin.admin_id, req.session.admin.username,
+            req.session.admin.position, req.session.admin.encrypted_password);
+        const {firstName,
+            lastName,
+            addressline1,
+            addressline2, 
+            addressbarangay,
+            addresscity,
+            addresscountry,
+            birthday,
+            gender,
+            contact,
+            status,
+            remarks,
+            medicalCondition,
+            voter,
+            pwd} = req.body;
+            const one = mainController.createAdminId(firstName);
+            const two = mainController.createAdminId(lastName);
+            const user_id = one+two;
+            const resident = new Resident(user_id,
+                                            firstName,
+                                            lastName,
+                                            contact,
+                                            birthday,
+                                            gender,
+                                            addressline1,
+                                            addressline2,
+                                            addressbarangay,
+                                            addresscountry,
+                                            voter,
+                                            pwd,
+                                            medicalCondition,
+                                            status,
+                                            remarks,
+                                            new Date(),
+                                            admin.getUsername());
+            admin_connection.query("INSERT INTO masterlist (resident_id, fname, lname, contact, birthdate, gender, age, address, is_voter, is_pwd, medical_condition, status, remarks, created_by) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)",[
+                resident.getResidentId(),
+                resident.getFname(),
+                resident.getLname(),
+                resident.getContactNum(),
+                resident.getBirthdate(),
+                resident.getGender(),
+                resident.getAge(),
+                resident.getAddress().getFullAddress(),
+                resident.getIsVoter(),
+                resident.getIsPwd(),
+                resident.getMedicalCondition().getDisease(),
+                resident.getStatus(),
+                resident.getRemarks(),
+                admin.getUsername()
+
+            ], (err, results) => {
+                if (err) console.log(err);
+                else {
+                    res.render('admin/admin-add-residents', {AlertMessage: "Resident Recorded"})
+                }
+            })
+    }else{
+        res.redirect('/');
+    }   
 })
 
 module.exports = app;
